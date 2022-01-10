a7fdc63c94338f2ea124f157948eba990ad5b57a
Abishek G <abishek.g@mrcooper.com>
2022-01-10 17:42:19 +0530

    Configures G-analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 
-import Document, {
-  Html, Head, Main, NextScript,
-} from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
   render() {
@@ -10,6 +8,22 @@ class MyDocument extends Document {
             <Html lang="en">
                 <Head>
                     <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet"/>
+                    <script
+                        async
+                        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+                    />
+                    <script
+                        dangerouslySetInnerHTML={{
+                          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+                        }}
+                    />
                 </Head>
                 <body>
                 <Main/>
