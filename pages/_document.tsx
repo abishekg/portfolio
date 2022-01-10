de995c988e17ca31e8e973df92f8de01cbd0a5d1
Abishek G <abishek.g@mrcooper.com>
2022-01-10 18:05:54 +0530

    configures google analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,35 +1,41 @@
 import React from 'react';
 
+import * as firebase from 'firebase/functions';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
   render() {
+    // @ts-ignore
+    const trackingId: string = firebase.config().config.pass;
     return (
-            <Html lang="en">
-                <Head>
-                    <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet"/>
-                    <script
-                        async
-                        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-                    />
-                    <script
-                        dangerouslySetInnerHTML={{
-                          __html: `
+      <Html lang="en">
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter&display=swap"
+            rel="stylesheet"
+          />
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${trackingId}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+            gtag('config', '${trackingId}', {
               page_path: window.location.pathname,
             });
           `,
-                        }}
-                    />
-                </Head>
-                <body>
-                <Main/>
-                <NextScript/>
-                </body>
-            </Html>
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
     );
   }
 }
